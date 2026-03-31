7ebf8e4f8c794304bc82ebac1e6b4ef2e297e3d1
CarsonRasmussen <carsonrasmussen@u.boisestate.edu>
2026-03-31 17:54:42 -0600

    Add files via upload

---
+++ dff.v
@@ -0,0 +1,17 @@
+module dff(
+    input D,
+    input Clock,
+    output reg Q,
+    output NotQ
+);
+
+    initial begin
+        Q <= 0;
+    end
+    
+    always @(posedge Clock)
+        Q <= D;
+
+    assign NotQ = ~Q;
+    
+endmodule
